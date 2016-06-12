30d33a75ebe5f25ac7827a7ccb862f811e66049a
Stefanie Epp <stefanie.epp@stonybrook.edu>
2016-06-12 16:12:06 -0400

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.jsoup.nodes.TextNode;
-
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
@@ -27,25 +26,58 @@ public class WikiPhilosophy {
 	 * @param args
 	 * @throws IOException
 	 */
+	static ArrayList alreadyBeen = new ArrayList();
+	
 	public static void main(String[] args) throws IOException {
 		
         // some example code to get you started
 
 		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
+
+		for (int i = 0; i < 10; i++) {
+			if (alreadyBeen.contains(url)) {
+				return;
+			} else {
+				alreadyBeen.add(url);
+			}
+			
+			System.out.println("Fetching..." + url);
+			Elements paragraphs = wf.fetchWikipedia(url);
+			WIkiParser wp = new WIkiParser(paragraphs);
+			Element e = wp.findFirstLink();
+
+			if (e == null) {
+				System.err.println("Got to a page with no valid links.");
+				return;
+			}
+			
+			System.out.println("Page: " + e.text());
+			url = e.attr("abs:href");
+			
+			if (url.equals("https://en.wikipedia.org/wiki/Philosophy")) {
+				System.out.println("Yay!");
+				break;
+			}
+		}
+		
+		
+		/*Elements paragraphs = wf.fetchWikipedia(url);
+		Elements links = paragraphs.select("a[href]");
 
 		Element firstPara = paragraphs.get(0);
+		Element firstLink = links.get(0);
 		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
+		Iterable<Node> iter = new WikiNodeIterable(firstLink);
 		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
+			//if (node instanceof TextNode && (!(((CharSequence)node).charAt(0) == '(')) && not italicized) {
+				System.out.println(node);
+				
 			}
-        }
+        }*/
 
         // the following throws an exception so the test fails
         // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+        //String msg = "Complete this lab by adding your code and removing this statement.";
+        //throw new UnsupportedOperationException(msg);
 	}
 }
